a53ea52b2c86727b44d3b6e91c8ade5427b36fac
bae-sh <b5460881@naver.com>
2021-08-11 22:29:15 +0900

    App start

---
+++ EmotionaryApp/src/navigations/Tab.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Flower } from "../screens/Flower";
+import { Camera } from "../screens/Camera";
+import { Calendar } from "../screens/Calendar";
+const Tab = createBottomTabNavigator();
+
+const TabNavigation = () => {
+  return (
+    <Tab.Navigator
+      initialRouteName="Flower"
+      tabBarOptions={{
+        inactiveTintColor: "#C1C1C1",
+        activeTintColor: "#000000",
+      }}
+    >
+      <Tab.Screen name="Flower" component={Flower} />
+      <Tab.Screen name="Camera" component={Camera} />
+      <Tab.Screen name="Calendar" component={Calendar} />
+    </Tab.Navigator>
+  );
+};
+
+export default TabNavigation;
