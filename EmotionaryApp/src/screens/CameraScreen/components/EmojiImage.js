955c370ee084bbe55ed68b9ed6f77f61e184b5dc
bae-sh <b5460881@naver.com>
2021-08-27 05:09:54 +0900

    add select other emotion

--- EmotionaryApp/src/screens/CameraScreen/components/EmojiImage.js
+++ EmotionaryApp/src/screens/CameraScreen/components/EmojiImage.js
@@ -1,16 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 import { images } from "../../../images";
-const Container = styled.View`
+const Container = styled.TouchableOpacity`
   margin: 20px auto;
 `;
 const Emoji = styled.Image`
   width: 100px;
   height: 100px;
 `;
-const EmojiImage = () => {
+const EmojiImage = ({ onPress }) => {
   return (
-    <Container>
+    <Container onPress={onPress}>
       <Emoji source={images.emoji}></Emoji>
     </Container>
   );
