7604d30f3185c4dfc52bfe6577c24b880c989a3f
SeongHyeon Bae <hyeon1293@gmail.com>
2021-11-10 23:06:45 +0900

    Delete key.js

--- EmotionaryApp/amplify/key.js
+++
@@ -1,6 +0,0 @@
-export const key = {
-  accessKeyId: process.env.ACCESSKEY_ID,
-  secretAccessKey: process.env.SECRETACCESS_KEY,
-  region: "ap-southeast-2",
-  bucket: "amplify-emotionaryapp-dev-31457-deployment",
-};
