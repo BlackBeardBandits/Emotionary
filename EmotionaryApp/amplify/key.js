cd79654ca1336e0b196d947373b88f40556e1902
SeongHyeon Bae <hyeon1293@gmail.com>
2021-11-10 23:05:44 +0900

    Update key.js

--- EmotionaryApp/amplify/key.js
+++ EmotionaryApp/amplify/key.js
@@ -1,6 +1,3 @@
 export const key = {
-  accessKeyId: "AKIAR5QEB4G6DLKJCOO7",
-  secretAccessKey: "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",
-  region: "ap-southeast-2",
-  bucket: "amplify-emotionaryapp-dev-31457-deployment",
+  //아마존 키값
 };
